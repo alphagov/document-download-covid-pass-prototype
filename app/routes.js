075e2faeafd6b31f699871dc300dac623c7011d5
Katie Smith <katie.smith@digital.cabinet-office.gov.uk>
2022-06-17 15:46:38 +0100

    Add routes and pages to send an email
    This changes the content on the index page, adds a page to send an email
    then shows a confirmation page when it is submitted.
    
    No errors are shown and no validation is done on the email address.

--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,23 @@ const express = require('express');
 
 const router = express.Router();
 
+const NotifyClient = require('notifications-node-client').NotifyClient;
+
+const notifyClient = new NotifyClient(process.env.API_KEY);
+
 // Add your routes here - above the module.exports line
+
+router.post('/email-address', (req, res) => {
+    notifyClient
+        .sendEmail(
+          templateId='77168d68-46a1-44fb-90ea-c52173122c5d',
+          emailAddress=req.body.emailAddress,
+          {reference: 'covid pass email'}
+        )
+        .then(response => console.log(response))
+        .catch(err => console.error(err))
+
+    res.redirect('/confirmation');
+  });
 
 module.exports = router;
