31909436c0b3db8a9a455f908af69f15112291fc
Chris Hill-Scott <me@quis.cc>
2022-06-23 14:22:31 +0100

    Add posted ema il to document download allow list

--- app/routes.js
+++ app/routes.js
@@ -7,6 +7,8 @@ const NotifyClient = require('notifications-node-client').NotifyClient;
 
 const notifyClient = new NotifyClient(process.env.API_KEY);
 
+const axios = require('axios');
+
 // Add your routes here - above the module.exports line
 
 router.post('/email-address', (req, res) => {
@@ -18,6 +20,18 @@ router.post('/email-address', (req, res) => {
         )
         .then(response => console.log(response))
         .catch(err => console.error(err))
+
+    axios
+      .post('https://documents.cloudapps.digital/allow-email', {
+        'email-address': req.body.emailAddress
+      })
+      .then(res => {
+        console.log(`statusCode: ${res.status}`);
+        console.log(res);
+      })
+      .catch(error => {
+        console.error(error);
+      });
 
     res.redirect('/confirmation');
   });
